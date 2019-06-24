84bc2c997fcff1f1f0098acb3b2e783fd819cc8c
Julian Orinyol <julianorinyol@gmail.comm>
2019-06-24 23:20:04 +0200

    alter index.js to take wrap app with provider + store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { saveState, loadState }  from './helpers/localStorage'
+import throttle from 'lodash/throttle';
+import studyCards from './reducers/StudyCards'
+const persistedState = loadState()
+
+let store = createStore(studyCards, persistedState)
+
+store.subscribe(throttle(() => {
+  saveState( store.getState() );
+}, 1000));
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
