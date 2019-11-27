446a5f0eabd1a1018ff509597296e40a79c58879
Julian Orinyol <julianorinyol@gmail.comm>
2019-11-27 16:56:16 +0100

    tests: add .env.test and refactor filmsPage e2e test

--- src/test/EndToEndTests/filmsPage.e2e.test.js
+++ src/test/EndToEndTests/filmsPage.e2e.test.js
@@ -1,16 +1,19 @@
 const puppeteer = require('puppeteer');
+const url = process.env.REACT_APP_FRONT_END_URL
+const headless = process.env.REACT_APP_END_TO_END_TESTS_HEADLESS 
 
 describe('FilmsPage', () => {
   it('should show 2 film cards', async () => {
+    const filmCardClass = '.FilmCard'
     let browser = await puppeteer.launch({
-      headless: true
+      headless
     });
     let page = await browser.newPage();
 
-    await page.goto('http://localhost:3000');
+    await page.goto(url);
     await page.waitFor(1000)
-    await page.waitForSelector('.FilmCard')
-    let elems = await page.$$('.FilmCard')
+    await page.waitForSelector(filmCardClass)
+    let elems = await page.$$(filmCardClass)
     expect(elems.length).toEqual(2)
 
     browser.close();
